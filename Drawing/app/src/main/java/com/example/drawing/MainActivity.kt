e5dd1bca573f8152e85cfabe8001001f5689a773
Jokubas Akramas <j.akramas@gmail.com>
2021-03-17 15:12:29 +0200

    Storage permissions

--- Drawing/app/src/main/java/com/example/drawing/MainActivity.kt
+++ Drawing/app/src/main/java/com/example/drawing/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.drawing
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.drawing.databinding.ActivityMainBinding
@@ -27,13 +31,21 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
-        binding.ibBrush.setOnClickListener{
+        binding.ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        binding.ibGallery.setOnClickListener {
+            if (isReadStorageAllowed()) {
+
+            } else {
+                requestStoragePermission()
+            }
         }
     }
 
-    fun paintClicked(view: View){
-        if(view !== mImageButtonCurrentPaint){
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
             binding.drawingView.setColor(colorTag)
@@ -47,23 +59,75 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showBrushSizeChooserDialog(){
+    private fun showBrushSizeChooserDialog() {
         binding_brush = DialogBrushSizeBinding.inflate(layoutInflater)
         val brushDialog = Dialog(this)
         brushDialog.setContentView(binding_brush.root)
         brushDialog.setTitle("Brush size: ")
-        binding_brush.ibSmallBrush.setOnClickListener{
+        binding_brush.ibSmallBrush.setOnClickListener {
             binding.drawingView.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
-        binding_brush.ibMediumBrush.setOnClickListener{
+        binding_brush.ibMediumBrush.setOnClickListener {
             binding.drawingView.setSizeForBrush(20.toFloat())
             brushDialog.dismiss()
         }
-        binding_brush.ibLargeBrush.setOnClickListener{
+        binding_brush.ibLargeBrush.setOnClickListener {
             binding.drawingView.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
         brushDialog.show()
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).toString()
+            )
+        ) {
+            Toast.makeText(
+                this, "Need permission to add a Background image",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), STORAGE_PERMISION_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(
+                    this@MainActivity, "Permision granted now you can read the storage files",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity, "Oops you just denied the permission.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean {
+        var result =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val STORAGE_PERMISION_CODE = 1
     }
 }
