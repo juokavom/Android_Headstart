d028d2d1496ef0039a7327decce6c7c77bcfbc01
Jokubas Akramas <j.akramas@gmail.com>
2021-03-18 11:43:11 +0200

    Workout sequence with timer

--- 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.example.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.example.a7minuteworkout.databinding.ActivityExerciseBinding
 
@@ -10,6 +11,11 @@ class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +28,12 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
         setupRestView()
+
+        exerciseList = Constants.defaultExerciseList()
     }
 
     override fun onDestroy() {
-        if(restTimer != null){
+        if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
         }
@@ -34,26 +42,65 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun setRestProgressBar() {
         binding.progressBar.progress = restProgress
-        restTimer = object : CountDownTimer(10000, 1000) {
+        restTimer = object : CountDownTimer(2000, 1000) {
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity,
-                    "Here now we will start the exercise.",
-                    Toast.LENGTH_SHORT).show()
+                currentExercisePosition++
+                setupExerciseView()
             }
 
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
                 binding.progressBar.progress = 10 - restProgress
-                binding.tvTimer.text = (10-restProgress).toString()
+                binding.tvTimer.text = (10 - restProgress).toString()
             }
         }.start()
     }
 
-    private fun setupRestView(){
-        if(restTimer != null){
+    private fun setExerciseProgressBar() {
+        binding.progressBarExercise.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(4000, 1000) {
+            override fun onFinish() {
+                if(currentExercisePosition < exerciseList?.size!! - 1){
+                    setupRestView()
+                }else{
+                    Toast.makeText(
+                        this@ExerciseActivity,
+                        "Congratulations! You have completed the 7 minutes workout!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding.progressBarExercise.progress = 30 - exerciseProgress
+                binding.tvExerciseTimer.text = (30 - exerciseProgress).toString()
+            }
+        }.start()
+    }
+
+    private fun setupRestView() {
+        binding.llRestView.visibility = View.VISIBLE
+        binding.llExerciseView.visibility = View.GONE
+
+        if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
         }
         setRestProgressBar()
+    }
+
+    private fun setupExerciseView() {
+        binding.llRestView.visibility = View.GONE
+        binding.llExerciseView.visibility = View.VISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+        setExerciseProgressBar()
+
+        binding.ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding.tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
     }
 }
