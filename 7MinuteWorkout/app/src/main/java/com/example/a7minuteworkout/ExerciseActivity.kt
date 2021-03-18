42edd11e772d611e7ae4e1c88817cdbe2bcdecc1
Jokubas Akramas <j.akramas@gmail.com>
2021-03-18 10:50:24 +0200

    Timer countdown

--- 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -2,23 +2,58 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minuteworkout.databinding.ActivityExerciseBinding
-import com.example.a7minuteworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbarExerciseActivity)
-        val actionbar = supportActionBar
-        if(actionbar != null){
-            actionbar.setDisplayHomeAsUpEnabled(true)
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.toolbarExerciseActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+        setupRestView()
+    }
+
+    override fun onDestroy() {
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.tvTimer.text = (10-restProgress).toString()
+            }
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
     }
 }
