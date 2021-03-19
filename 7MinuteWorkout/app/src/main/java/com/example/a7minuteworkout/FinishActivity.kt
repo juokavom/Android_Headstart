f96e4c71868be48301dd17e9f3658cf23718d54a
Jokubas Akramas <j.akramas@gmail.com>
2021-03-19 11:16:59 +0200

    SQlite db and date of finished exercise appending

--- 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
+++ 7MinuteWorkout/app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
@@ -2,7 +2,9 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.a7minuteworkout.databinding.ActivityFinishBinding
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFinishBinding
@@ -21,5 +23,19 @@ class FinishActivity : AppCompatActivity() {
         binding.btnFinish.setOnClickListener{
             finish()
         }
+
+        addDateToDataBase()
+    }
+    private fun addDateToDataBase(){
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.i("DATE:", "$dateTime")
+        val sdf = java.text.SimpleDateFormat("yyyy MMM dd HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        val dbHandler = SqliteOpenHelper(this, null)
+        dbHandler.addDate(date)
+        Log.i("DATE:", "Added")
+
     }
 }
