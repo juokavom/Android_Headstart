dbf815216816792e5bbe58f5fb0bf30facf7a000
Jokubas Akramas <j.akramas@gmail.com>
2021-03-20 14:00:22 +0200

    Recycler view binding with ViewBinding and not with findViewById

--- HappyPlaces/app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
+++ HappyPlaces/app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.happyplaces.R
+import com.example.happyplaces.databinding.ItemHappyPlaceBinding
 import com.example.happyplaces.models.HappyPlaceModel
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -16,30 +17,40 @@ open class HappyPlacesAdapter(
     private var list: ArrayList<HappyPlaceModel>
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MyViewHolder(
-            LayoutInflater.from(context).inflate(
-                R.layout.item_happy_place,
-                parent,
-                false
-            )
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemHappyPlaceBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val model = list[position]
 
-        if (holder is MyViewHolder) {
-            holder.itemView.findViewById<CircleImageView>(R.id.iv_place_image)
-                .setImageURI(Uri.parse(model.image))
-            holder.itemView.findViewById<TextView>(R.id.tvTitle).text = model.title
-            holder.itemView.findViewById<TextView>(R.id.tvDescription).text = model.description
-        }
+        if(holder is MyViewHolder) holder.bind(model)
+
+//        if (holder is MyViewHolder) {
+//            holder.ivPlaceImage.setImageURI(Uri.parse(model.image))
+//            holder.tvTitle.text = model.title
+//            holder.tvDescription.text = model.description
+//        }
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
+//
+//    private class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val ivPlaceImage = view.findViewById(R.id.iv_place_image) as CircleImageView
+//        val tvTitle = view.findViewById(R.id.tvTitle) as TextView
+//        val tvDescription = view.findViewById(R.id.tvDescription) as TextView
+//    }
 
-    private class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    class MyViewHolder(private val binding: ItemHappyPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(model: HappyPlaceModel){
+            binding.ivPlaceImage.setImageURI(Uri.parse(model.image))
+            binding.tvTitle.text = model.title
+            binding.tvDescription.text = model.description
+        }
+    }
 }
