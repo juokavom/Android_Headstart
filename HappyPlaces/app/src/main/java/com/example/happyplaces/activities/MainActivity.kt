c2011708981ffa5db7a7be61150df601357cde54
Jokubas Akramas <j.akramas@gmail.com>
2021-03-20 13:21:00 +0200

    Fetched item binding to recycler view

--- HappyPlaces/app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ HappyPlaces/app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.databinding.ActivityMainBinding
 import com.example.happyplaces.models.HappyPlaceModel
@@ -21,15 +24,27 @@ class MainActivity : AppCompatActivity() {
         }
         getHappyPlacesListFromLocalDB()
     }
+
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>){
+        binding.rvHappyPlacesList.layoutManager = LinearLayoutManager(this)
+        binding.rvHappyPlacesList.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+
+        binding.rvHappyPlacesList.adapter = placesAdapter
+    }
+
     private fun getHappyPlacesListFromLocalDB(){
         val dbHandler = DatabaseHandler(this)
         val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
         if(getHappyPlaceList.size > 0){
-            getHappyPlaceList.map { i ->
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
-            }
+            binding.rvHappyPlacesList.visibility = View.VISIBLE
+            binding.tvNoRecordsAvailable.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
+        }else{
+            binding.rvHappyPlacesList.visibility = View.GONE
+            binding.tvNoRecordsAvailable.visibility = View.VISIBLE
         }
     }
 }
