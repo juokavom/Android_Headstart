6d7646017bbde49300bd81227df599c30245cd84
Jokubas Akramas <j.akramas@gmail.com>
2021-03-23 10:22:28 +0200

    Custom font installation

--- Projemanag/app/src/main/java/com/example/projemanag/SplashActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -1,11 +1,31 @@
 package com.example.projemanag
 
+import android.graphics.Typeface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import com.example.projemanag.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        Api < 30
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+//        Api >= 30
+//        window.insetsController?.hide(WindowInsets.Type.statusBars())
+
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding.tvAppName.typeface = typeFace
     }
 }
