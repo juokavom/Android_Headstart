cf5b2a7905b58e0c352438b6bfffdd1b9fd87514
Jokubas Akramas <j.akramas@gmail.com>
2021-03-23 17:47:23 +0200

    Auto login

--- Projemanag/app/src/main/java/com/example/projemanag/activities/SplashActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import com.example.projemanag.databinding.ActivitySplashBinding
+import com.example.projemanag.firebase.FirestoreClass
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
@@ -25,7 +26,12 @@ class SplashActivity : AppCompatActivity() {
         binding.tvAppName.typeface = typeFace
 
         Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
+            var currentUserID = FirestoreClass().getCurrentUserId()
+            if(currentUserID.isNotEmpty()){
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, IntroActivity::class.java))
+            }
             finish()
         }, 2500)
     }
