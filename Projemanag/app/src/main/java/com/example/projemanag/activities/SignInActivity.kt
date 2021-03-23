85fd4a2d2edbec4216f9590ed1495bc3f03c570c
Jokubas Akramas <j.akramas@gmail.com>
2021-03-23 16:19:04 +0200

    Sign in to firebase

--- Projemanag/app/src/main/java/com/example/projemanag/activities/SignInActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/SignInActivity.kt
@@ -1,12 +1,21 @@
 package com.example.projemanag.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +23,55 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
         setUpActionBar()
 
+        auth = FirebaseAuth.getInstance()
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        binding.btnSignIn.setOnClickListener {
+            signInRegisteredUser()
+        }
+
+        setUpActionBar()
+    }
+
+    private fun signInRegisteredUser() {
+        val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+        val password: String = binding.etEmail.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            showProgressDialog(getString(R.string.please_wait))
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        Log.d("Sign in", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Sign in", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter password")
+                false
+            }
+            else -> true
+        }
     }
 
     private fun setUpActionBar() {
--- Projemanag/app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -8,6 +8,8 @@ import android.view.WindowManager
 import android.widget.Toast
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUpActivity : BaseActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -33,11 +35,27 @@ class SignUpActivity : BaseActivity() {
         val password: String = binding.etPassword.text.toString().trim{ it <= ' '}
 
         if(validateForm(name, email, password)){
-            Toast.makeText(
-                this,
-                "Now we can register a new user",
-                Toast.LENGTH_SHORT
-            ).show()
+            showProgressDialog(getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email!!
+                        Toast.makeText(
+                            this, "$name you have successfully " +
+                                    "registered the email address $registeredEmail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this, "Registration failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 
